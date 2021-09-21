81166a33f02ded0130012c2b6852ed8384580514
Riznokril <48068349+Riznokril@users.noreply.github.com>
2021-09-22 02:46:35 +0300

    Added counters of swaps, comparisions and sorting time

--- heap_sort.py
+++ heap_sort.py
@@ -1,7 +1,9 @@
-def swap(arr, i, j):
+def swap(arr, i, j, counters):
     arr[i], arr[j] = arr[j], arr[i]
+    counters[0]+=1
+    return counters
 
-def heapify(arr, n, i, order):
+def heapify(arr, n, i, order, counters):
     left = 2*i + 1
     right = 2*i + 2
     largest = i
@@ -9,31 +11,42 @@ def heapify(arr, n, i, order):
     if order == "asc":    
         if left < n and arr[i] < arr[left]:
             largest = left
+            counters[1]+=1
 
         if right < n and arr[largest] < arr[right]:
             largest = right
+            counters[1]+=1
         
         if largest != i:
-            swap(arr, i, largest)        
-            heapify(arr, n, largest, order)
+            swap(arr, i, largest, counters)                
+            heapify(arr, n, largest, order, counters)
     else:
         if left < n and arr[i] > arr[left]:
             largest = left
+            counters[1]+=1
 
         if right < n and arr[largest] > arr[right]:
             largest = right
+            counters[1]+=1
         
         if largest != i:
-            swap(arr, i, largest)        
-            heapify(arr, n, largest, order)
+            swap(arr, i, largest, counters)        
+            heapify(arr, n, largest, order, counters)
 
 def heap_sort(arr, order):
     n = len(arr)
+    counter_swaps = 0
+    counter_compares = 0
+    counters = [counter_swaps, counter_compares]
 
     for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i, order)
+        heapify(arr, n, i, order, counters)
   
     for i in range(n-1, 0, -1):
-        swap(arr, 0, i)   
-        heapify(arr, i, 0, order)
+        swap(arr, 0, i, counters)   
+        heapify(arr, i, 0, order, counters)
+    
+    return counters
+
+
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from heap_sort import heap_sort
+from timeit import default_timer as time
 
 if __name__ == '__main__':
 
@@ -8,7 +9,11 @@ if __name__ == '__main__':
     print("Input asc or desc")
     order = input()
 
-    heap_sort(arr, order)
-    
+    start_time = time()
+    counters = heap_sort(arr, order)
+    sort_time = (float(time() - start_time))*1000
+    print("Sorting time:", sort_time,"ms" )
+    print("Swaps:", counters[0])
+    print("Comparisons", counters[1])
     for i in range(len(arr)):
         print (arr[i], end = ' ')
